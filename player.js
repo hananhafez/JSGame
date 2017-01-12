461f50502ea641b5672c546882d6dedda20644e5
KhaledGamal <khaledgamal@localhost.localdomain>
2017-01-12 18:02:10 +0200

    first stable version

---
+++ player.js
@@ -0,0 +1,50 @@
+
+var player = function  (name) {
+	var src , carX;
+	this.name = name;
+
+	Object.defineProperty(this, 'src', {configurable:true,
+		enumerable:true,
+
+		set:function (n) {
+			src = n;	
+		},
+
+		get:function () {
+			return this.src;
+		}
+	});
+
+	Object.defineProperty(this, 'carX', {configurable:true,
+		enumerable:true,
+		set:function (n) {
+			carX = n;	
+		},
+		get:function () {
+			return this.carX;
+		}
+	});
+	this.move = function (event) {
+		switch (event.keyCode) {
+			var car1 = window.getElementById('car1');
+			case 37:
+				//var car1 = window.getElementById('car1');
+				
+				car1.style.marginLeft=parseInt(car1.style.marginLeft) - 5 + 'px';
+				
+				break;
+			case 39:
+				//var car1 = window.getElementById('car1');
+				
+				car1.style.marginLeft=parseInt(car1.style.marginLeft) - 5 + 'px';
+				
+				break;
+		}
+		
+
+	}
+	this.src = src;
+	this.carX = carX;
+
+};
+
